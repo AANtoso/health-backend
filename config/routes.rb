dfc1c2044b3ce8d8708f6f5944e8c6141310f5b7
AANtoso <adjoaan@gmail.com>
2020-06-29 17:24:15 -0400

    Fixed typo.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-        resources: :healths
+        resources :healths
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
