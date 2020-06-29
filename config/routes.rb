42fda837001a06d21ea0f192f23362a137aeee48
AANtoso <adjoaan@gmail.com>
2020-06-29 17:23:02 -0400

    Configured routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   resources :healths
+  namespace :api do
+    namespace :v1 do
+      resources :users
+        resources: :healths
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
