4220c55477ad88842d74a957a21a25b066123d49
AANtoso <adjoaan@gmail.com>
2020-06-29 17:11:13 -0400

    Created CRUD functions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,32 +2,34 @@ class Api::V1::UsersController < ApplicationController
     before_action :set_user
 
     def index
-    
+        users = User.all
+        render json: users, status: 200
     end
 
     def create
+        user = User.new(user_params)
 
+        if user.save 
+            session[:user_id] = user.id 
+            render json: UserSerializer.new(user), status: 200
+        else
+            resp = {
+                error: users.error.full_messaged.to_sentence
+            }
+            render json: user.errors, status: 400
+        end
     end
 
     def show
-
-    end
-
-    def update
-    
-    end
-
-    def destroy
-    
+        user_json = UserSerializer.new(user).serialized_json
+        render json: user_json
     end
 
     private
 
     def set_user
-    
     end
 
     def user_params
-    
     end
 end
