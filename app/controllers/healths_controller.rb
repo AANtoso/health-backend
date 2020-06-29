ceb67d02766359377e059e892e57cf7ba9c4bd06
AANtoso <adjoaan@gmail.com>
2020-06-29 15:01:54 -0400

    Created controller skeletons.

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -1,2 +1,33 @@
-class HealthsController < ApplicationController
+class Api::V1::HealthsController < ApplicationController
+    before_action :set_health
+
+    def index
+    
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def update
+    
+    end
+
+    def destroy
+    
+    end
+
+    private
+
+    def set_health
+    
+    end
+
+    def health_params
+    
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+    before_action :set_user
+
+    def index
+    
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def update
+    
+    end
+
+    def destroy
+    
+    end
+
+    private
+
+    def set_user
+    
+    end
+
+    def user_params
+    
+    end
 end
