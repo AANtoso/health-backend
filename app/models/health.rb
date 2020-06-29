0e694b04a737ad35b53ec1f52ea59d89b633aac6
AANtoso <adjoaan@gmail.com>
2020-06-29 19:54:48 -0400

    Added validations.

--- app/models/health.rb
+++ app/models/health.rb
@@ -1,3 +1,6 @@
 class Health < ApplicationRecord
     has_many :medications
+
+    validates :diagnosis, presence: true
+    validates :diagnosis, uniqueness: true
 end
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,4 +1,6 @@
 class Medication < ApplicationRecord
     belongs_to :health
-    
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
