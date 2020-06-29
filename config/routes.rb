331cc34c38dc2e80eb193fb96f8f1cf7e6e113bc
AANtoso <adjoaan@gmail.com>
2020-06-29 19:11:56 -0400

    Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :medications
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :medications do
         resources :healths
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
