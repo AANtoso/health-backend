884ddd6e8aa383d2fe04996ecbe7c1b9db31d90a
AANtoso <adjoaan@gmail.com>
2020-06-29 17:12:38 -0400

    Created private methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,10 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def set_user
+        user = User.find(parans[:id])
     end
 
     def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
