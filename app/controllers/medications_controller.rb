0078dbef7806e0a99a17628500f44f50637fbd1e
AANtoso <adjoaan@gmail.com>
2020-06-29 19:39:18 -0400

    Added controller and CRUD actions.

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,46 @@
-class MedicationsController < ApplicationController
+class Api::V1::MedicationsController < ApplicationController
+    before_action :set_medication, only: [:index, :show]
+    def index
+        @medications = @health.medications 
+        render json: @medications.to_json(include: [:health]), status: 200
+    end
+
+    def create
+        @health = Health.find(params[:health_id])
+        @medication = @health.medications.build(medication_params)
+        if @medication.save
+            render json: @health.to_json(include: [:medications]), status: 200
+        else
+            render json: {error: "MEDICATION NOT SAVED"}
+        end
+    end
+
+    def show
+        @medication = Medication.find_by(id:params[:id])
+        render ison: @medication.to_json(include: [:health]), status: 200
+    end
+
+    def update
+        @medication = Medication.find_by(id:params[:id])
+        @health = Health.find(@medication.health_id)
+        @medication.update(medication_params)
+        render json: @health.to_json(include: [:medications]), status: 200
+    end
+
+    def destroy
+        @medication = Medication.find_by(id:params[:id])
+        @health = Health.find(@medication.health_id)
+        @medication.destroy
+            render json: @health.to_json(include: [:medications]), status: 200
+    end
+
+    private
+
+    def set_medication
+        @medication = Medication.find(params[:id])
+    end
+
+    def medication_params
+        params.require(:medication).permit(:name, :dose, :frequency)
+    end
 end
